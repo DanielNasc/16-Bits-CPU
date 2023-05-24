240465ddd7a1fb09fa68eb1d059dbd1fd2fa5b6f
Sahinake <lumaraujo1992@hotmail.com>
2023-05-24 10:12:24 -0300

    reajusted code

--- Bangtan.asm
+++ Bangtan.asm
@@ -4,24 +4,32 @@
 
 .text
 main:
-    lj 	 $jn0, 	vetor       	# carrega o endereço base do vetor em $t0
-    ld 	 $jn1, 	tamanho     	# carrega o tamanho do vetor em $t1
-    jc 	 $jn1, 	-1   		# subtrai 1 do tamanho para obter o índice máximo
+    lj 	vetor       	# carrega o endereço base do vetor em $R1
+    ia  $jn0
+    lda tamanho     	# carrega o tamanho do vetor em R2
+    ia  $jn1
+    jc  $jn1, -1   	    # subtrai 1 do tamanho para obter o índice máximo
 
-    ld 	 $jn2, 	($jn0)      	# carrega o primeiro elemento do vetor em $t2 (maior valor inicial)
-    jc   $jn0, 	4    		# avança para o próximo elemento do vetor
+    ld 	0($jn0)      	# carrega o primeiro elemento do vetor em $R3 (maior valor inicial)
+    ia $jn2
+    jc $jn0, 4    	    # avança para o próximo elemento do vetor
 loop:
-    yibi $jn1,  end       	# se o tamanho for zero, termina o loop
-    ld 	 $jn3, 	0($jn0)      	# carrega o próximo elemento do vetor em $t3
+    i   $iu,  $jn1
+    cmpi    0
+    geb	end       	    # se o tamanho for zero, termina o loop
+    ld 	0($jn0)      	# carrega o próximo elemento do vetor em $R4
+    ia $jn3
 
-    migs $jn4	$jn3, 	$jn2   	# compara se $t3 é menor que $t2
-    yabi $jn4, 	not_greater	# se $t3 não for maior que $t2, pula para not_greater
-    i	 $jn2, 	$jn3       	# se $t3 for maior que $t2, atualiza o valor máximo
+    i   $iu,  $jn2
+    cmp $jn3                # seta carry flag se A >= R4
+    geby    not_greater	    # se $R4 não for maior que $R3, pula para not_greater
+    i   $jn2, $jn3       	# se $R4 for maior que $R3, atualiza o valor máximo
 
 not_greater:
-    jc 	$jn0, 	4    		# avança para o próximo elemento do vetor
-    jc 	$jn1, 	-1   		# decrementa o tamanho
-    d 	 loop              	# retorna para o início do loop
+    jc 	$jn0, 4    	    # avança para o próximo elemento do vetor
+    jc 	$jn1, -1   	    # decrementa o tamanho
+    d 	loop            # retorna para o início do loop
 
 end:
+   
    
