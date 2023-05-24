bfc690d3d8e6545915bec798aa26fe00d05721f2
DanielNasc <danielnasc15987@gmil.com>
2023-05-24 10:05:05 -0300

    Simple Assembler: split tokens

--- Assembler.py
+++ Assembler.py
@@ -1,8 +1,84 @@
-def main():
-    f = open("code.asm", "r");
+class Assembler:
+    context = None
+    program = ""
+
+    ints = ["lj," "lw", "ld", "jd", "jc", "dl", "jci", "geb",
+            "ggb","yibi", "migs", "yabi", "i", "d"]
+    dtypes = ["word"]
+
+    varMetadata = {}
+    RAMmemory = []
+
+    def __init__(self) -> None:
+        pass
+
+    def execute(self, file):
+        f = open(file, "r")
+
+        for line in f:
+            self.handle_line(line)
+
+        f.close()
+
+        mem_file = open("outRAM.mem", "w")
 
 
-    f.close()
+        mem_file.close()
+
+    def handle_line(self, line):
+        line = line.strip()
+        if (len(line) == 0): return
+
+        # new context definition
+        if (line[0] == "."):
+            new_ctx = line[1:]
+            self.change_context(new_ctx)
+        # read variables and its values
+        elif (self.context == "data"):
+            self.handle_data(line)
+        # read instructions
+        elif (self.context == "inst"):
+            self.handle_insts(line)
+    
+    def handle_data(self, line):
+        tokens = line.split(" ")
+
+        if (len(tokens) < 3 
+            or tokens[0][len(tokens[0]) - 1] != ":"
+            or tokens[1] not in self.dtypes
+        ):
+            raise Exception("Wrong syntax: ", tokens)
+        
+        values = tokens[2:]
+    
+        for value in values:
+            if (not self.check_type(value, tokens[1])):
+                raise Exception("wrong type \"" + value + "\" in " + str(tokens) + " not " + tokens[1])
+
+    
+    def check_type(self, value, type):
+        try:
+            if (type == "word"):
+                int(value)
+            return True
+        except ValueError:
+            return False
+
+
+    def handle_insts(self, line):
+        tokens = line.split(" ")
+
+        if (len(tokens) < 2
+            or (tokens[0][0] != "." and tokens[0] not in self.ints)
+        ):
+            raise Exception("Wrong syntax:: inst")
+        
+
+    def change_context(self, new_ctx):
+        self.context = new_ctx
+        print("New Context >>", new_ctx)
+
 
 if __name__ == "__main__":
-    main()
+    assembler = Assembler()
+    assembler.execute("Bangtan.asm")
--- Bangtan.asm
+++ Bangtan.asm
@@ -1,27 +1,27 @@
 .data
-    vetor: .word 2, 8, 6, 23, 3, 9, 4   # vetor de exemplo
-    tamanho: .word 8                     # tamanho do vetor
+    vetor: word 2 8 6 23 3 9 4
+    tamanho: word 8
 
 .text
 main:
-    lj 	 $jn0, 	vetor       	# carrega o endereço base do vetor em $t0
-    ld 	 $jn1, 	tamanho     	# carrega o tamanho do vetor em $t1
-    jc 	 $jn1, 	-1   		# subtrai 1 do tamanho para obter o índice máximo
+    lj 	 $jn0 	vetor
+    ld 	 $jn1 	tamanho
+    jc 	 $jn1 	-1
 
-    ld 	 $jn2, 	($jn0)      	# carrega o primeiro elemento do vetor em $t2 (maior valor inicial)
-    jc   $jn0, 	4    		# avança para o próximo elemento do vetor
+    ld 	 $jn2 	($jn0)
+    jc   $jn0 	4
 loop:
-    yibi $jn1,  end       	# se o tamanho for zero, termina o loop
-    ld 	 $jn3, 	0($jn0)      	# carrega o próximo elemento do vetor em $t3
+    yibi $jn1  end
+    ld 	 $jn3 	0($jn0)
 
-    migs $jn4	$jn3, 	$jn2   	# compara se $t3 é menor que $t2
-    yabi $jn4, 	not_greater	# se $t3 não for maior que $t2, pula para not_greater
-    i	 $jn2, 	$jn3       	# se $t3 for maior que $t2, atualiza o valor máximo
+    migs $jn4	$jn3 	$jn2
+    yabi $jn4 	
+    i	 $jn2 	$jn3
 
 not_greater:
-    jc 	$jn0, 	4    		# avança para o próximo elemento do vetor
-    jc 	$jn1, 	-1   		# decrementa o tamanho
-    d 	 loop              	# retorna para o início do loop
+    jc 	$jn0 	4
+    jc 	$jn1 	-1
+    d 	 loop
 
 end:
    
